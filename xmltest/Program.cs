de633792a506e2b39d1167c749f5ae577b175c4a
Jakob <js@jakobsenkl.pw>
2020-07-25 13:29:41 +0200

    add xmltest solution
    this is temporary.

---
+++ xmltest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+using System.Linq;
+
+
+namespace xmltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlcontent = null;
+
+            using (var wc = new WebClient())
+            {
+                xmlcontent = wc.DownloadString("https://www.nvidia.com/Download/API/lookupValueSearch.aspx?TypeID=3");
+            }
+            var xDoc = XDocument.Parse(xmlcontent);
+
+            var names = xDoc.Descendants("Name");
+            foreach (var name in names)
+            {
+                string sname = name.Value.ToString();
+                if (sname == "GeForce RTX 2080") 
+                {
+                    string value = name.Parent.Value;
+                    int index = value.IndexOf(sname);
+                    string cleanValue = (index < 0)
+                        ? value
+                        : value.Remove(index, sname.Length);
+
+                    Console.WriteLine(cleanValue);
+                }
+            }
+        }
+    }
+}
