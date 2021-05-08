dea07706ace42226db042d5a45a46ede4e56ea42
fyr77 <login@notjakob.com>
2021-05-08 12:45:39 +0200

    Update licenses

--- EnvyUpdate/InfoWindow.xaml.cs
+++ EnvyUpdate/InfoWindow.xaml.cs
@@ -25,48 +25,5 @@ namespace EnvyUpdate
         {
             System.Diagnostics.Process.Start("https://github.com/fyr77/EnvyUpdate/");
         }
-
-        private void text_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            this.Cursor = Cursors.Hand;
-        }
-
-        private void text_MouseLeave(object sender, MouseEventArgs e)
-        {
-            this.Cursor = Cursors.Arrow;
-        }
-
-        private void textEnvyUpdate_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/fyr77/EnvyUpdate/blob/master/LICENSE");
-        }
-
-        private void textFody_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/Fody/Fody/blob/master/License.txt");
-        }
-
-        private void textCostura_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/Fody/Costura/blob/master/license.txt");
-        }
-
-        private void textNotifyIcon_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/hardcodet/wpf-notifyicon/blob/master/LICENSE");
-        }
-
-        private void textNotifications_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/Federerer/Notifications.Wpf/blob/master/LICENSE");
-        }
-        private void textNewtonsoft_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/JamesNK/Newtonsoft.Json/blob/master/LICENSE.md");
-        }
-        private void textResourceEmbedder_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/MarcStan/resource-embedder/blob/master/LICENSE");
-        }
     }
 }
