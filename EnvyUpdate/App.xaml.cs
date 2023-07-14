58bef7537fa4784fff9483e5b26dc8089feb8740
Jakob <gh@notjakob.com>
2023-07-14 12:55:18 +0200

    edit dark theme implementation

--- EnvyUpdate/App.xaml.cs
+++ EnvyUpdate/App.xaml.cs
@@ -19,13 +19,16 @@ namespace EnvyUpdate
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            // Remove placeholder light theme and apply system app theme
             Wpf.Ui.Appearance.Accent.ApplySystemAccent();
+            Application.Current.Resources.MergedDictionaries.RemoveAt(0);
             ThemesDictionary themedict = new ThemesDictionary();
             if (Util.IsDarkTheme())
                 themedict.Theme = Wpf.Ui.Appearance.ThemeType.Dark;
             else
                 themedict.Theme = Wpf.Ui.Appearance.ThemeType.Light;
             Application.Current.Resources.MergedDictionaries.Add(themedict);
+            // TODO: Watch for theme changes and dynamically update
 
             // Listen to notification activation
             ToastNotificationManagerCompat.OnActivated += toastArgs =>
